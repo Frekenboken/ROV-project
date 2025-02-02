d3ed8d202796b432f14b3f12aa4037d794ebffed
Pavel Ovchinnikov <pavel.ov07@mail.ru>
2025-02-02 22:46:27 +0300

    update

--- device/main2.py
+++ device/main2.py
@@ -7,7 +7,6 @@ import pynmea2
 import RPi.GPIO as gpio
 import time
 import psutil
-from queue import Queue
 import zmq
 import cv2
 import base64
@@ -16,18 +15,17 @@ import numpy as np
 
 from PID import PIDController
 
+
 def main():
+    print("Initialization... (5 sec)")
+
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
 
     cap = cv2.VideoCapture(0)
 
-    try:
-        ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
-    except Exception as e:
-        print("Error:", e)
-        ser = None
+    ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
 
     depth_pid = PIDController(0.01, 0.0001, 0.1)
     depth_pid.set_limit(10, 170)
@@ -48,6 +46,15 @@ def main():
 
     data = []
 
+    for i in range(10):
+        if ser.in_waiting > 0:
+            break
+        time.sleep(0.5)
+    else:
+        raise Exception
+
+    print("Ready!")
+
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -61,14 +68,22 @@ def main():
         message = socket.recv_json()
         print(f"Received control signal: {message}")
 
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            arduino_data = line.split(",")
+            print(f"Received: {line}")
+
         # Отправляем изображение клиенту
         socket.send(jpg_as_text, flags=zmq.SNDMORE)
-        socket.send_json({"cpu_temperature": psutil.sensors_temperatures(),
-                          "cpu_usage": int(psutil.cpu_percent())})
-
-        if ser and ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(f"Received: {line}")
+        socket.send_json({"cpu_temperature": psutil.sensors_temperatures()["cpu_thermal"][0][1],
+                          "cpu_usage": int(psutil.cpu_percent()),
+                          "depth": arduino_data[0],
+                          "gx": arduino_data[0],
+                          "gy": arduino_data[0],
+                          "in_temperature": arduino_data[0],
+                          "in_humidity": arduino_data[0],
+                          "in_pressure": arduino_data[0],
+                          "out_temperature": arduino_data[0]})
 
         # STEP
         # depth_speed = depth_pid.compute(get_sensors('depth'), get_sensors('depth_target'))
@@ -83,6 +98,7 @@ def main():
     cap.release()
     socket.close()
     context.term()
+
 
 if __name__ == "__main__":
     main()
