2bfe1617e69c4394c8cdf14cd19ebf491459d737
Pavel Ovchinnikov <pavel.ov07@mail.ru>
2025-02-05 16:29:22 +0300

    update

--- device/main2.py
+++ device/main2.py
@@ -184,10 +184,10 @@ def control_send():
             x_value = float(response['x'])
             y_value = float(response['y'])
 
-            set_angle(servo_pwm_instances[0], map_value(get_value(-x_value, -y_value), 0, 1, 70, 110))
-            set_angle(servo_pwm_instances[1], map_value(get_value(x_value, -y_value), 0, 1, 70, 110))
-            set_angle(servo_pwm_instances[2], map_value(get_value(-x_value, y_value), 0, 1, 70, 110))
-            set_angle(servo_pwm_instances[3], map_value(get_value(x_value, y_value), 0, 1, 70, 110))
+            set_angle(servo_pwm_instances[0], map_value(get_value(-x_value, -y_value), 0, 1, 80, 100))
+            set_angle(servo_pwm_instances[1], map_value(get_value(x_value, -y_value), 0, 1, 80, 100))
+            set_angle(servo_pwm_instances[2], map_value(get_value(-x_value, y_value), 0, 1, 80, 100))
+            set_angle(servo_pwm_instances[3], map_value(get_value(x_value, y_value), 0, 1, 80, 100))
 
 
         except KeyboardInterrupt:
