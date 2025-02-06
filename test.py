e84dac733c284496ef845e219b850b04039e0e62
Pavel Ovchinnikov <pavel.ov07@mail.ru>
2025-02-06 23:30:36 +0300

    update

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
-import glob
+from rpi_bad_power import new_under_voltage
 
-def get_input_voltage():
-    hwmon_paths = glob.glob("/sys/class/hwmon/hwmon*/in*_input")
-    for path in hwmon_paths:
-        try:
-            with open(path, "r") as f:
-                voltage = int(f.read().strip()) / 1000  # Преобразуем в вольты
-                if 4.5 < voltage < 5.5:  # Фильтруем напряжение в диапазоне 5V
-                    return voltage
-        except (FileNotFoundError, ValueError):
-            continue
-    return None
-
-voltage = get_input_voltage()
-if voltage:
-    print(f"Напряжение питания (USB-C): {voltage:.3f} В")
+under_voltage = new_under_voltage()
+if under_voltage is None:
+    print("System not supported.")
+elif under_voltage.get():
+    print("Under voltage detected.")
 else:
-    print("Не удалось получить напряжение питания")
+    print("Voltage is normal.")
